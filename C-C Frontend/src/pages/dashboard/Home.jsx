514fcaf7d3daf9c94f1ca721d2099b279805d24b
NevilleBarla <nevillebarla02@gmail.com>
2026-03-11 12:30:09 +0530

    Reduced levels

--- C-C Frontend/src/pages/dashboard/Home.jsx
+++ C-C Frontend/src/pages/dashboard/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import BASE_URL from "../../../config";
+import BASE_URL from "../../config";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
--- C-C Frontend/src/pages/dashboard/ResumeAnalyzer.jsx
+++ C-C Frontend/src/pages/dashboard/ResumeAnalyzer.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import BASE_URL from "../../../config";
+import BASE_URL from "../../config";
 
 function ResumeAnalyzer() {
   const [file, setFile] = useState(null);
