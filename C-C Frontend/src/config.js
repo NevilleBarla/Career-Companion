d8127a0a662ff88e66746c45f5c85e4a2cc28f4f
NevilleBarla <nevillebarla02@gmail.com>
2026-03-12 20:40:30 +0530

    Website is now live

--- C-C Frontend/src/config.js
+++ C-C Frontend/src/config.js
@@ -3,6 +3,6 @@
 // Change this ONE line when you deploy your backend
 // -------------------------------------------------------
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://career-companion-4c2c.onrender.com";
 
 export default BASE_URL;
